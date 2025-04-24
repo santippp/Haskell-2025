be8bf1c652d190568c9aef404a7f1f160446c492
santippp <spriccofrakich@gmail.com>
2025-04-23 22:37:04 -0300

    Arboles 1

--- practica1.hs
+++ practica1.hs
@@ -28,3 +28,26 @@ insertar c (xs, p) = ((take p xs ++ [c] ++ drop p xs), p+1)
 borrar :: Linea -> Linea
 borrar (xs, p) | p > 0 = ((take p xs ++ drop (p+1) xs), p-1)
                | otherwise = (xs, p)
+
+data Bin a = Hoja | Nodo (Bin a) a (Bin a) deriving (Show, Eq)
+
+member :: Ord a => a -> Bin a -> Bool
+member _ Hoja = False
+member a (Nodo l x r) | a == x = True
+                      | a < x = member a l
+                      | otherwise = member a r
+
+minimum' :: Bin a -> a
+minimum' (Nodo Hoja x r) = x
+minimum' (Nodo l x r) = minimum' l
+
+maximum' :: Bin a -> a
+maximum' (Nodo l x Hoja) = x
+maximum' (Nodo l x r) = maximum' r
+
+checkBST :: Ord a => Bin a -> Bool
+checkBST Hoja = True
+checkBST (Nodo Hoja x Hoja) = True
+checkBST (Nodo Hoja x l@(Nodo ll lx lr)) = x > lx && maximum' l < x && checkBST l
+checkBST (Nodo r@(Nodo rl rx rr) x Hoja) = x > rx && minimum' r < x && checkBST r
+checkBST (Nodo l@(Nodo _ lx _) x r@(Nodo _ rx _)) = x > lx && x < rx && maximum' l < x && minimum' r > x && checkBST l && checkBST r
